0e86e35a9dd94d30fb4bf8aa85c671c27093635e
Victor Olamide <victor.olamide@example.com>
2026-01-09 10:30:41 -0800

    feat: update interface

--- contracts/interfaces/IEventTicket.sol
+++ contracts/interfaces/IEventTicket.sol
@@ -10,7 +10,6 @@ interface IEventTicket {
     struct Event {
         uint256 eventId;
         string name;
-        string description;
         uint256 date;
         string venue;
         uint256 totalTickets;
@@ -18,8 +17,6 @@ interface IEventTicket {
         uint256 ticketPrice;
         address organizer;
         bool isActive;
-        uint256 maxTransferPrice;
-        bool transferable;
     }
     
     struct Ticket {
@@ -28,36 +25,32 @@ interface IEventTicket {
         address originalBuyer;
         bool isUsed;
         uint256 seatNumber;
-        string qrCode;
     }
     
     event EventCreated(uint256 indexed eventId, string name, address organizer);
     event TicketMinted(uint256 indexed ticketId, uint256 indexed eventId, address buyer);
     event TicketVerified(uint256 indexed ticketId, address verifier);
-    event TicketTransferred(uint256 indexed ticketId, address from, address to);
     
     function createEvent(
-        string memory _name,
-        string memory _description,
+        string calldata _name,
         uint256 _date,
-        string memory _venue,
+        string calldata _venue,
         uint256 _totalTickets,
-        uint256 _ticketPrice,
-        uint256 _maxTransferPrice,
-        bool _transferable
+        uint256 _ticketPrice
     ) external returns (uint256);
     
     function mintTicket(
         uint256 _eventId,
-        uint256 _seatNumber,
-        string memory _qrCode
+        uint256 _seatNumber
     ) external payable returns (uint256);
     
     function verifyTicket(uint256 _ticketId) external returns (bool);
     
-    function events(uint256 _eventId) external view returns (Event memory);
+    function getEvent(uint256 _eventId) external view returns (Event memory);
     
-    function tickets(uint256 _ticketId) external view returns (Ticket memory);
+    function getTicket(uint256 _ticketId) external view returns (Ticket memory);
     
-    function verifiedTickets(uint256 _ticketId) external view returns (bool);
+    function totalSupply() external view returns (uint256);
+    
+    function totalEvents() external view returns (uint256);
 }
