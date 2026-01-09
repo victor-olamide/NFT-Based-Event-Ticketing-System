05ba96a0e4299f2c43168982e356058056cab677
Victor Olamide <victor.olamide@example.com>
2026-01-09 10:16:51 -0800

    feat: add event and ticket metadata structures with mappings

--- contracts/EventTicket.sol
+++ contracts/EventTicket.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -9,10 +10,37 @@ import "@openzeppelin/contracts/utils/Counters.sol";
  * @title EventTicket
  * @dev ERC721 NFT contract for event tickets with Base network optimization
  */
-contract EventTicket is ERC721, Ownable {
+contract EventTicket is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     
     Counters.Counter private _tokenIds;
+    Counters.Counter private _eventIds;
+    
+    struct Event {
+        uint256 eventId;
+        string name;
+        uint256 date;
+        string venue;
+        uint256 totalTickets;
+        uint256 ticketsSold;
+        uint256 ticketPrice;
+        address organizer;
+        bool isActive;
+    }
+    
+    struct Ticket {
+        uint256 ticketId;
+        uint256 eventId;
+        address originalBuyer;
+        bool isUsed;
+        uint256 seatNumber;
+    }
+    
+    mapping(uint256 => Event) public events;
+    mapping(uint256 => Ticket) public tickets;
+    
+    event EventCreated(uint256 indexed eventId, string name, address organizer);
+    event TicketMinted(uint256 indexed ticketId, uint256 indexed eventId, address buyer);
     
     constructor() ERC721("EventTicket", "ETKT") {}
     
