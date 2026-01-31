e04adde12700c96be2047005a0075c56eb8d76b8
victor-olamide <olaboyevictor20@gmail.com>
2026-01-31 01:09:36 -0800

    "feat-add-eventPricingTierTicketsSold-mapping"

--- contracts/EventTicket.sol
+++ contracts/EventTicket.sol
@@ -75,6 +75,7 @@ contract EventTicket is ERC721, ERC721URIStorage, Ownable, ReentrancyGuard {
     mapping(uint256 => Event) public events;
     mapping(uint256 => PricingTier[]) public eventPricingTiers;
     mapping(uint256 => string[]) public eventPricingTierNames;
+    mapping(uint256 => mapping(uint256 => uint256)) public eventPricingTierTicketsSold;
     mapping(uint256 => mapping(address => uint256[])) public eventUserTickets;
     mapping(uint256 => mapping(address => bool)) public eventAuthorizedStaff;
     mapping(uint256 => uint256) public ticketToPricingTier;
