f785fc795925a0d0b37fb21901d39ad30ef5b2ff
victor-olamide <olaboyevictor20@gmail.com>
2026-01-31 01:10:02 -0800

    "feat-add-eventPricingTierMaxSupplies-mapping"

--- contracts/EventTicket.sol
+++ contracts/EventTicket.sol
@@ -77,6 +77,7 @@ contract EventTicket is ERC721, ERC721URIStorage, Ownable, ReentrancyGuard {
     mapping(uint256 => string[]) public eventPricingTierNames;
     mapping(uint256 => mapping(uint256 => uint256)) public eventPricingTierTicketsSold;
     mapping(uint256 => mapping(uint256 => uint256)) public eventPricingTierPrices;
+    mapping(uint256 => mapping(uint256 => uint256)) public eventPricingTierMaxSupplies;
     mapping(uint256 => mapping(address => uint256[])) public eventUserTickets;
     mapping(uint256 => mapping(address => bool)) public eventAuthorizedStaff;
     mapping(uint256 => uint256) public ticketToPricingTier;
